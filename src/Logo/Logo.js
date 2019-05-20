e8a0955cc29a1b949eb3487ea877913be460b21e
Chris O'Donnell <dev@codfish.io>
2019-05-20 12:51:18 -0400

    fix: use internal Link in Logo component

--- src/Logo/Logo.js
+++ src/Logo/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import withStyles from '@material-ui/core/styles/withStyles';
+import Link from '../Link';
 
 function Logo(props) {
   const { theme, classes, href } = props;
