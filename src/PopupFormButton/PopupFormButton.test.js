ebf0a65a966272bd02a43f66a8f68f3ddc614b68
Chris O'Donnell <dev@codfish.io>
2019-06-01 15:31:59 -0400

    test: bump react-testing-library to v8

--- src/PopupFormButton/PopupFormButton.test.js
+++ src/PopupFormButton/PopupFormButton.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
+import { render, fireEvent } from '@testing-library/react';
 import AddCircle from '@material-ui/icons/AddCircle';
 import PopupFormButton from './PopupFormButton';
 
--- src/WorkflowStatus/WorkflowStatus.test.js
+++ src/WorkflowStatus/WorkflowStatus.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render } from '@testing-library/react';
 import WorkflowStatus from './WorkflowStatus';
 
 describe('WorkflowStatus', () => {
