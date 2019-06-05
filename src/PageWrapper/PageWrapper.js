771e80fb324f6364d1acb7f4796eac15a77dcde8
Chris O'Donnell <dev@codfish.io>
2019-06-04 23:24:43 -0400

    fix: update spacing prop in PageWrapper to be material v4 compatible

--- src/PageWrapper/PageWrapper.js
+++ src/PageWrapper/PageWrapper.js
@@ -19,13 +19,13 @@ const PageWrapper = ({ children, spacing, className, ...props }) => {
 
 PageWrapper.propTypes = {
   children: PropTypes.node.isRequired,
-  spacing: PropTypes.oneOf([0, 8, 16, 24, 32, 40]),
+  spacing: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
   className: PropTypes.string,
 };
 
 PageWrapper.defaultProps = {
   className: '',
-  spacing: 24,
+  spacing: 3,
 };
 
 export default PageWrapper;
