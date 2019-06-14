f166fd80075a62214dca70fa8b81c15c7fd81c80
Yaniv Sofer <yaniv.sofer@ey.com>
2019-06-14 14:43:59 -0400

    feat: update subtitle component to accept a class

--- src/SubTitle/SubTitle.js
+++ src/SubTitle/SubTitle.js
@@ -1,36 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
+import classnames from 'classnames';
 import useStyles from './styles';
 
-const SubTitle = ({ children, label }) => {
+const SubTitle = ({ children, label, className }) => {
   const classes = useStyles();
+  const classNames = classnames(className);
+
   return (
-    <React.Fragment>
-      {label && (
-        <Typography
-          className={classes.label}
-          variant="overline"
-          gutterBottom
-          data-testid="subtitle-label"
-        >
-          {`${label}: `}
-        </Typography>
-      )}
-      <Typography data-testid="subtitle-content" variant="subtitle1" gutterBottom>
-        {children}
-      </Typography>
-    </React.Fragment>
+    <Typography className={classNames} variant="subtitle1" gutterBottom>
+      {label && <span data-testid="subtitle-label" className={classes.label}>{`${label}: `}</span>}
+      <span data-testid="subtitle-content">{children}</span>
+    </Typography>
   );
 };
 
 SubTitle.propTypes = {
   label: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.array, PropTypes.node]).isRequired,
+  className: PropTypes.string,
 };
 
 SubTitle.defaultProps = {
   label: null,
+  className: null,
 };
 
 export default SubTitle;
--- src/SubTitle/styles.js
+++ src/SubTitle/styles.js
@@ -3,7 +3,8 @@ import { makeStyles } from '@material-ui/styles';
 const useStyles = makeStyles(() => ({
   label: {
     fontWeight: 'bold',
-    paddingRight: '1rem',
+    paddingRight: '0.25rem',
+    fontVariant: 'small-caps',
   },
 }));
 
